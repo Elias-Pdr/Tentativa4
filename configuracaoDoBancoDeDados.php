460f5125170d2d78ec3b3a6528f1c6671bbc5bba
Pedro Elias <106087744+Elias-Pdr@users.noreply.github.com>
2022-09-23 19:22:19 -0300

    Tentativa4

---
+++ configuracaoDoBancoDeDados.php
@@ -0,0 +1,11 @@
+<?php
+
+$hostDoBancoDeDados = 'localhost';
+$nomeDoBancoDeDados = 'crud_php';
+$usuarioDoBancoDeDados = 'root';
+$senhaDoBancoDeDados = '';
+
+
+$mysqli = mysqli_connect($hostDoBancoDeDados, $usuarioDoBancoDeDados, $senhaDoBancoDeDados, $nomeDoBancoDeDados); 
+ 
+?>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+include_once("configuracaoDoBancoDeDados.php");
+
+$result = mysqli_query($mysqli, "SELECT * FROM usuarios ORDER BY id DESC"); 
+
+?>
+
+<html>
+<head>	
+	<title>Página Inicial</title>
+</head>
+
+<body>
+<a href="adicionar.html">Adicionar Novo Dado</a><br/><br/>
+
+	<table width='80%'>
+
+	<tr>
+		<td>Nome</td>
+		<td>Idade</td>
+		<td>Email</td>
+		<td>Atualizar</td>
+	</tr>
+	<?php 
+	
+	while($res = mysqli_fetch_array($result)) { 		
+		echo "<tr>";
+		echo "<td>".$res['nome']."</td>";
+		echo "<td>".$res['idade']."</td>";
+		echo "<td>".$res['email']."</td>";	
+		echo "<td><a href=\"editar.php?id=$res[id]\">Editar</a> | <a href=\"deletar.php?id=$res[id]\" onClick=\"return confirm('Você tem certeza que deseja deletar?')\">Deletar</a></td>";		
+	}
+	?>
+	</table>
+</body>
+</html>
